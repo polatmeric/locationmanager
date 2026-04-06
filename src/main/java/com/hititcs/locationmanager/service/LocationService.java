398cdaef171688b89a223745e26a8e9cda9dd837
Meric Polat <polatmeric@gmai.com>
2026-04-06 22:31:27 +0300

    some fixes

--- src/main/java/com/hititcs/locationmanager/service/LocationService.java
+++ src/main/java/com/hititcs/locationmanager/service/LocationService.java
@@ -4,6 +4,7 @@ import com.hititcs.locationmanager.model.*;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.DataClassRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -17,15 +18,17 @@ public class LocationService {
     @Cacheable(value = "regions", key = "#isoCountry ?: 'ALL'")
     public List<Region> listRegions(String isoCountry) {
         if (isoCountry == null || isoCountry.isBlank()) {
-            return jdbcTemplate.query("SELECT * FROM regions", new BeanPropertyRowMapper<>(Region.class));
+            return jdbcTemplate.query("SELECT * FROM regions",new DataClassRowMapper<>(Region.class));
+            //return jdbcTemplate.query("SELECT * FROM regions", new BeanPropertyRowMapper<>(Region.class));
         }
-        return jdbcTemplate.query("SELECT * FROM regions WHERE iso_country = ?", new BeanPropertyRowMapper<>(Region.class), isoCountry.toUpperCase());
+        return jdbcTemplate.query("SELECT * FROM regions WHERE iso_country = ?",new DataClassRowMapper<>(Region.class));
+        //return jdbcTemplate.query("SELECT * FROM regions WHERE iso_country = ?", new BeanPropertyRowMapper<>(Region.class), isoCountry.toUpperCase());
     }
 
     @Cacheable(value = "airports", key = "#isoCountry + '-' + #scheduledService")
     public List<Airport> listAirports(String isoCountry, String scheduledService) {
         return jdbcTemplate.query("SELECT * FROM airports WHERE iso_country = ? AND scheduled_service = ?", 
-                new BeanPropertyRowMapper<>(Airport.class), isoCountry.toUpperCase(), scheduledService.toLowerCase());
+                new DataClassRowMapper<>(Airport.class), isoCountry.toUpperCase(), scheduledService.toLowerCase());
     }
 
     @CacheEvict(value = {"regions", "airports"}, allEntries = true)
